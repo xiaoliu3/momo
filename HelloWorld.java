73b7e0f1f36f9760d6f0bf3334b267510857685b
Xiao Liu <liuxiao9087@gmail.com>
2017-12-09 16:37:54 -0500

    Revert "changed a line"
    This reverts commit 07fa462273ff7e4e87f1787d98d59e9b63b98e56.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,10 @@
 public class HelloWorld {
 
-    public String msg = "OMG It is snowing today!";
+    public String msg = "OMG";
 
     public static void main(String[] args) {
         
         System.out.println(msg);
-
         System.out.println( "I love momo" );
     }
 }
