07fa462273ff7e4e87f1787d98d59e9b63b98e56
Xiao Liu <liuxiao9087@gmail.com>
2017-12-09 16:31:37 -0500

    changed a line

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,11 @@
 public class HelloWorld {
 
-    public String msg = "OMG";
+    public String msg = "OMG It is snowing today!";
 
     public static void main(String[] args) {
         
         System.out.println(msg);
+
         System.out.println( "I love momo" );
     }
 }
